eda79c322b8578d00f6d31663afe09acfca4e62f
santiago.gomez1 <santiago.gomez1@alumnos.uneatlantico.es>
2025-03-20 12:28:01 +0100

    ADDING CV

--- portfolio/src/Components/FloatingCTA.jsx
+++ portfolio/src/Components/FloatingCTA.jsx
@@ -1,16 +1,26 @@
 import { FaFileDownload } from "react-icons/fa";
 
-const FloatingCTA = () => {
+const PDF_FILE = "/Santiago_Gómez_de_la_Torre_CV.pdf"
+
+export const FloatingCTA = () => {
+
+  const downloadFile = (url) => {
+    const fileName = url.split("/").pop();
+    const aTag = document.createElement("a")
+    aTag.href = url;
+    aTag.setAttribute("download", fileName)
+    document.body.appendChild(aTag)
+    aTag.click()
+    aTag.remove()
+  }
+
   return (
-    <a
-      href="../../public/Santiago Gómez de la Torre - CV.pdf"
-      download
+    <button onClick={() => downloadFile(PDF_FILE)}
       className="fixed bottom-8 right-8 z-50 flex items-center gap-2 rounded-full border border-stone-50/30 bg-stone-200 px-4 py-3 font-medium text-stone-900 shadow-lg transition-all hover:bg-stone-300 hover:shadow-xl"
     >
       <FaFileDownload className="text-xl" />
       <span>Download CV</span>
-    </a>
+    </button>
   );
 };
 
-export default FloatingCTA;
