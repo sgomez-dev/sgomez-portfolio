6c412baa55e9c8574b283f406b737d4792d8ae6c
Santiago Gómez de la Torre Romero <gomezdelatorre15@gmail.com>
2025-04-27 09:31:15 +0200

    UPDATE metadata generation to remove dynamic title and standardize it across all sections for consistency

--- sgomez/src/components/Metadata.jsx
+++ sgomez/src/components/Metadata.jsx
@@ -1,15 +1,15 @@
 export function generateMetadata({ title, description, keywords }) {
   return {
-    title: `${title} | Santiago Gómez de la Torre Romero Portfolio`,
+    title: `Santiago Gómez de la Torre Romero Portfolio`,
     description,
     keywords,
     openGraph: {
-      title: `${title} | Santiago Gómez de la Torre Romero Portfolio`,
+      title: `Santiago Gómez de la Torre Romero Portfolio`,
       description,
       type: 'website',
     },
     twitter: {
-      title: `${title} | Santiago Gómez de la Torre Romero Portfolio`,
+      title: `Santiago Gómez de la Torre Romero Portfolio`,
       description,
     },
   };
